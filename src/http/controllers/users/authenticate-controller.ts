2f94ddca2c2c58067e0cd71502ab01b64e4a1d07
Julio Machado <juliocarlos00@hotmail.com>
2025-03-23 23:16:48 +0000

    add/ RBAC + refresh token

--- src/http/controllers/users/authenticate-controller.ts
+++ src/http/controllers/users/authenticate-controller.ts
@@ -1,7 +1,4 @@
-import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
-import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -23,7 +20,9 @@ export async function authenticateController(
     const { user } = await authenticateUserCase.execute({ email, password });
 
     const token = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
@@ -31,9 +30,29 @@ export async function authenticateController(
       }
     );
 
-    return reply.status(200).send({
-      token,
-    });
+    const refreshToken = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      }
+    );
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { registerController } from "./register-controller";
 import { authenticateController } from "./authenticate-controller";
 import { profileController } from "./profile-controller";
+import { refreshController } from "./refresh-controller";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateController);
+
+  app.patch("/token/refresh", refreshController);
 
   /** Auth   */
   app.get("/me", { onRequest: [verifyJWT] }, profileController);
